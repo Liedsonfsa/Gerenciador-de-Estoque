f67f3a9429168c9867fbf374756c64162397e295
Raileal <railealyt@gmail.com>
2025-01-20 13:13:57 -0300

    Feito a função de inserir, não teve como testar, mas tá feito

--- Estoque.hs
+++ Estoque.hs
@@ -7,3 +7,12 @@ module Estoque (
     atualizarQuantidade,
     imprimirProdutosNoEstoque
 ) where
+
+-- Adiciona um produto ao estoque
+adicionarProduto :: [Produto] -> Produto -> [Produto]
+adicionarProduto estoque novoProduto =
+    if any (\p -> nome p == nome novoProduto) estoque
+        then map (\p -> if nome p == nome novoProduto 
+                        then p { quantidade = quantidade p + quantidade novoProduto }
+                        else p) estoque
+        else novoProduto : estoque
